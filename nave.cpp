dbed1b7391e05aed63828dfcbabced5eaacf69c0
alvarogonmar <alvarogonmar0@gmail.com>
2024-11-24 13:29:28 -0600

    naves finished

--- nave.cpp
+++ nave.cpp
@@ -2,8 +2,19 @@
 
 #include "nave.h"
 
-Nave::Nave(int tamanio)
+Nave::Nave(int tamanio, int x, int y)
 {
     this->tamanio = tamanio;
     this->impacto = 0; // iniciar impactos en contador 0
+    this->posicion_x = x;
+    this->posicion_y = y;
+}
+
+void Nave::impacto_recibido()
+{
+    impacto++;
+}
+bool Nave::hundido()
+{
+    return impacto >= tamanio;
 }
--- nave.h
+++ nave.h
@@ -8,7 +8,9 @@ class Nave
 public:
     int tamanio; // tamanio del barco
     int impacto; // contar los impactos recibidos
+    int posicion_x, posicion_y;
 
-    Nave(int tamanio); // constructor del tamanio del barco
-    bool hundido();    // Verificar si el barco esta hundido
+    Nave(int tamanio, int x, int y); // constructor del tamanio del barco
+    void impacto();
+    bool hundido(); // Verificar si el barco esta hundido
 };
