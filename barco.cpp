2b751cc2c7a5670feb584bf2dced68830676153d
alvarogonmar <alvarogonmar0@gmail.com>
2024-11-24 13:14:21 -0600

    Barco.cpp finished

---
+++ barco.cpp
@@ -0,0 +1,9 @@
+// Archivos de implementación (.cpp): Aquí implementas los métodos definidos en los encabezados.
+
+#include "barco.h"
+
+Barco::Barco(int tamanio)
+{
+    this->tamanio = tamanio;
+    this->impacto = 0; // iniciar impactos en contador 0
+}
